d56034ac0f0f32390ad74d43182df61560eae6e3
ajinkz <ajinkz@gmail.com>
2020-04-02 19:59:17 +0530

    init push

---
+++ newsite/polls/models.py
@@ -0,0 +1,6 @@
+from django.db import models
+from mongoengine import Document, fields
+
+class Poll(Document):
+    poll_name = fields.StringField(required=True)
+    poll_votes = fields.StringField(required=True)
